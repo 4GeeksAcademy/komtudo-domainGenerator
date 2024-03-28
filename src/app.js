6757c3f33a2c41cec8adfc3e7a62c5136f6d84cf
KomTudo <129621963+KomTudo@users.noreply.github.com>
2024-03-28 09:41:05 +0000

    Version 1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensionDomains = [".com", ".es", ".net", ".org"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let f = 0; f < adj.length; f++) {
+      for (let g = 0; g < noun.length; g++) {
+        for (let h = 0; h < extensionDomains.length; h++) {
+          console.log(pronoun[i] + adj[f] + noun[g] + extensionDomains[h]);
+        }
+      }
+    }
+  }
 };
